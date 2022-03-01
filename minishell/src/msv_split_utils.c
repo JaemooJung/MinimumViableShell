362b4402593fd83ad5d76f0d5e656457c68d0e08
Han Gyul Kim <hangyulkim@han.local>
2022-03-01 12:43:27 +0900

    edited parsing criteria from whitespaces to blank

--- minishell/src/msv_split_utils.c
+++ minishell/src/msv_split_utils.c
@@ -6,7 +6,7 @@
 /*   By: hakim <hakim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 20:14:49 by hakim             #+#    #+#             */
-/*   Updated: 2022/02/28 20:14:50 by hakim            ###   ########.fr       */
+/*   Updated: 2022/03/01 12:42:55 by hakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 bool	m_is_space(char c)
 {
-	if (c == SPACE || (9 <= c && c <= 13))
+	if (c == SPACE)
 		return (true);
 	return (false);
 }
