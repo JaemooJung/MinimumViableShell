ed56f279b6e754ad7986469d50f103e2e840acd2
Han Gyul Kim <hangyulkim@han.local>
2022-03-08 03:24:09 +0900

    can run without pipe but very restrictive yet

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -14,18 +14,10 @@ void	minimum_viable_shell(t_list *env)
 	if (parse_user_input(cmdline, &tree))
 		return ;
 	print_parsed(tree);
-	//	token = parse(cmdline);
-	//	run_tokens(token);
+	run_tokens(tree, env);
 	//free token, 
 	//free tree;
 	ft_free_str(cmdline);
-}
-
-void run_tokens(void)
-{
-	/*
-	 만약에 파이프가 있다면 -> ast 현재 노드의 오른쪽 노드가 존재한다
-	 */
 }
 
 int	main(int argc, char **argv, char **envp)
