e206edabd9313190e2e8529e838452e4e09eb4f6
JaemooJung <dbdbwer@naver.com>
2022-03-07 15:06:47 +0900

    please

--- minishell/inc/minishell.h
+++ minishell/inc/minishell.h
@@ -4,8 +4,11 @@
 # include <stdio.h>
 # include <stdbool.h>
 # include <stdlib.h>
-# include "parse_user_input.h"
+# include <string.h>
+# include <sys/errno.h>
+# include <unistd.h>
 
+# include "parse_user_input.h"
 # include "debug.h"
 
 # define SPACE ' '
@@ -14,6 +17,61 @@
 
 # define MALLOC_ERROR 1
 
+typedef struct s_list
+{
+	struct s_list	*next;
+	struct s_list	*prev;
+	char			*line;
+}	t_list;
 
+typedef enum e_status
+{
+	SUCCESS,
+	FAILURE,
+	MALLOC_ERR
+}	t_stat;
+
+/* envp friends */
+t_list	*envp_to_ours(char **envp);
+char	*get_value(t_list *env, char *key);
+t_stat	append_to_env(t_list *env, char *str);
+
+/* builtins */
+t_stat	mvs_echo(char **chunk, t_list *env);
+t_stat	mvs_exit(char **chunk, t_list *env); // is whitespace func necessary? + dequote have to be removed after parsing done
+t_stat	mvs_cd(char **chunk, t_list *env); // error handling to be done
+t_stat	mvs_pwd(char **chunk, t_list *env);
+t_stat	mvs_env(char **chunk, t_list *env);
+t_stat	mvs_export(char **chunk, t_list *env);
+t_stat	mvs_unset(char **chunk, t_list *env);
+
+/* utils */
+size_t	ft_strlen(const char *str);
+bool	m_is_space(char c);
+bool	m_is_quote(char c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *src);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atoi(const char *str);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_print_error(char *cmd, char *arg, char *error);
+void	ft_free_split(char **splitted, int index);
+void	*ft_free_str(char *str);
+void	dequote(char *str);
+char	**mvs_split(char const *s);
+
+/* list */
+t_list	*ft_lstnew(char *line);
+t_list	*ft_lstlast(t_list *lst);
+t_list	*copy_list(t_list *src);
+t_list	*sort_list(t_list *lst, int (*cmp)(const char *, const char *, size_t));
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	delete_node(t_list *lst);
+void	*ft_free_lst(t_list *lst);
 
 #endif
--- minishell/inc/parse_user_input.h
+++ minishell/inc/parse_user_input.h
@@ -1,5 +1,5 @@
-#ifndef AST_H
-# define AST_H
+#ifndef PARSE_USER_INPUT_H
+# define PARSE_USER_INPUT_H
 
 # define T_NULL 1
 # define T_PIPE 2
@@ -19,7 +19,7 @@
 # define LEFT 0
 # define RIGHT 1
 
-typedef struct s_ast_node t_ast_node;
+typedef struct s_ast_node	t_ast_node;
 
 typedef struct s_token
 {
@@ -35,20 +35,24 @@ typedef struct s_ast_node
 	t_ast_node		*right;
 }	t_ast_node;
 
-bool	m_is_space(char c);
-bool	m_is_quote(char c);
-void	ft_free_split(char **splitted, int index);
-char	**mvs_split(char const *s);
-char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char const *s1, char const *s2);
+bool		m_is_space(char c);
+bool		m_is_quote(char c);
+void		ft_free_split(char **splitted, int index);
+char		**mvs_split(char const *s);
+char		*ft_strdup(const char *s1);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*conv_str_join(char *argv, char *new_str);
+char		*make_argv(t_token **tokens);
+void		print_parsed(t_ast_node *tree);
 
 t_ast_node	*make_ast_node(int node_type, char *content);
 t_ast_node	*ast_insert(t_ast_node *root, t_token *token, int side);
 t_ast_node	*ast_insert_node(t_ast_node *root, t_ast_node *node, int side);
 
-int		lexer(char **splitted, t_token **tokens);
-int		parser(t_token **tokens, t_ast_node **tree);
+int			lexer(char **splitted, t_token **tokens);
+int			parse_pipeline(t_ast_node **root, t_token **tokens);
+int			parser(t_token **tokens, t_ast_node **tree);
 
-int		parse_user_input(const char *input, t_ast_node **tree);
+int			parse_user_input(const char *input, t_ast_node **tree);
 
 #endif
